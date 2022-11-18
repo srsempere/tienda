598d7b9a8a4c757dbfd13cc172e19b2f4471f6e3
samu <samueljose.garcia@iesdonana.org>
2022-11-18 19:28:44 +0100

    haciendo portal

--- public/index.php
+++ public/index.php
@@ -1,24 +1,59 @@
 <?php session_start() ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp">
     <title>Portal artículos.</title>
 </head>
+
 <body>
 
-<?php
+    <?php
     require('../src/admin-auxiliar.php');
 
 
-?>
-<h1 class="text-3xl font-bold underline text-blue-600">
-    Hello world!
-  </h1>
+    ?>
 
-<script src="/js/flowbite/flowbite.js"></script>
+    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">G∂D <span class="text-blue-600 dark:text-blue-500">⌨</span> G∊T</h1>
+    <div class="overflow-x-auto relative">
+            <h1 class="text-5xl font-extrabold dark:text-white"><small class="ml-2 font-semibold text-gray-500 dark:text-gray-400">Bienvenido a tu portal de compra</small></h1>
+    </div>
+
+    <div class="flex items-top justify-center min-h-screen bg-gray-900">
+        <div class="col-span-12">
+            <div class="overflow-auto lg:overflow-visible ">
+
+            </div>
+        </div>
+    </div>
+        .table {
+            border-spacing: 0 15px;
+        }
+
+        i {
+            font-size: 1rem !important;
+        }
+
+        .table tr {
+            border-radius: 20px;
+        }
+
+        tr td:nth-child(n+5),
+        tr th:nth-child(n+5) {
+            border-radius: 0 .625rem .625rem 0;
+        }
+
+        tr td:nth-child(1),
+        tr th:nth-child(1) {
+            border-radius: .625rem 0 0 .625rem;
+        }
+    </style>
+    <script src="/js/flowbite/flowbite.js"></script>
 </body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,php}",
+  "./src/**/_*.{html,php}",
   "./node_modules/flowbite/**/*.js"] ,
   theme: {
     extend: {},
